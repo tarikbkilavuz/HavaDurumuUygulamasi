14e7fe00a099ed8bcbde665571e716525abb204a
tarikbkilavuz <94147815+tarikbkilavuz@users.noreply.github.com>
2023-12-06 23:42:59 +0300

    Add files via upload

---
+++ HavaDurumuUygulamasi/HavaDurumuUygulamasi/ForecastData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HavaDurumuUygulamasi
+{
+	public class ForecastData
+	{
+		public string Description { get; set; }
+		public string Temperature { get; set; }
+		public string Wind { get; set; }
+		public string Day { get; set; }
+		
+	}
+}
+
---
+++ HavaDurumuUygulamasi/HavaDurumuUygulamasi/WeatherData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HavaDurumuUygulamasi
+{
+	public class WeatherData
+	{
+		public string Description { get; set; }
+		public string Temperature { get; set; }
+		public string Wind { get; set; }
+		public ForecastData[] Forecast { get; set; }
+	}
+}
+
